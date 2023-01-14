c6fcf76f63c3414bde4a2d4295d38e01461bdf58
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2023-01-14 12:30:07 +0500

    First commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 let hero = document.querySelector('.hero');
+let about = document.querySelector('.skills');
+let skills = document.querySelector('.about');
 let menuBtn = document.querySelector('.menu');
 let siteHeader = document.querySelector('.mobile-nav')
 
 menuBtn.addEventListener('click', () => {
     hero.classList.toggle('blur');
+    skills.classList.toggle('blur');
+    about.classList.toggle('blur');
     siteHeader.classList.toggle('height');
-    menuBtn.classList.toggle('menu-bg')
+    menuBtn.classList.toggle('menu-bg');
 })
